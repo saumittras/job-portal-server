5cb0b4669d67b6d68b470b1d7c336357b32dbe17
Saumittra Sarker <saumittra.eee@gmail.com>
2025-03-03 23:43:10 +0600

    added mongoDB to server

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// api section start from here
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s8hxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// api section end from here
+
+app.get("/", (req, res) => {
+  res.send("Jobportal Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Job Server is runing successfully at port : ${port}`);
+});
