b37ed7de3c605fd435d4d82c26315e116f916f7c
Saumittra Sarker <saumittra.eee@gmail.com>
2025-03-05 09:59:00 +0600

    added my job application api and aggration

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
 
@@ -34,11 +34,54 @@ async function run() {
 
     // Job Related API
     const jobsCollection = client.db("jobHub").collection("jobs");
+    const applicationCollection = client
+      .db("jobHub")
+      .collection("job_applications");
 
     // to load all jobs data
     app.get("/jobs", async (req, res) => {
       const result = await jobsCollection.find().toArray();
       // console.log(result);
+      res.send(result);
+    });
+
+    // API to get job Details
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    //Job application API
+    app.post("/job-applications", async (req, res) => {
+      const newApplication = req.body;
+      console.log(newApplication);
+      const result = await applicationCollection.insertOne(newApplication);
+      console.log(result);
+      res.send(result);
+    });
+
+    // get my Job Application
+    app.get("/job-application", async (req, res) => {
+      const email = req.query.email;
+      const query = { applicant_email: email };
+      const result = await applicationCollection.find(query).toArray();
+
+      // simple aggreget data
+      for (const application of result) {
+        console.log(application.job_id);
+        const query_1 = { _id: new ObjectId(application.job_id) };
+        const job = await jobsCollection.findOne(query_1);
+
+        if (job) {
+          application.job = job.title;
+          application.company = job.company;
+          application.location = job.location;
+          application.company_logo = job.company_logo;
+        }
+      }
+
       res.send(result);
     });
   } finally {
